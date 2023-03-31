098613ddd4d749d6c0315039feaac1d10c4379bf
Ayush11 <ayushraj315@gmail.com>
2023-03-31 17:13:32 +0530

    updated

--- OnlineWalletApplication/src/main/java/com/wallet/onlinewalletapplication/OnlineWalletApplication.java
+++ OnlineWalletApplication/src/main/java/com/wallet/onlinewalletapplication/OnlineWalletApplication.java
@@ -10,17 +10,14 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "Online Wallet Application", version = "1.1"),
-        security = { @SecurityRequirement(name = "basicAuth")
-}
+@OpenAPIDefinition(info = @Info(title = "Online wallet Application", version = "1.1"),
+        security = { @SecurityRequirement(name = "basicAuth"),
+                @SecurityRequirement(name = "bearerToken") },
+        servers = { @Server(url = "/", description = "Default Server URL") }
 
 )
 
-@SecuritySchemes({
-        @SecurityScheme(name = "basicAuth",
-                type = SecuritySchemeType.HTTP, scheme = "basic")
-}
-)
+@SecuritySchemes({ @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"), @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT") })
 @SpringBootApplication
 public class OnlineWalletApplication {
 
